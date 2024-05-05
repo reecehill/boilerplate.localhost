2c1aeb6fe49d87ec20872299be30591457ab3101
Reece Hill <29638128+reecehill@users.noreply.github.com>
2024-05-05 23:09:06 +0100

    Vue3+TS5+Pinia2+TanStack5+Vite5

---
+++ env.d.ts
@@ -0,0 +1,3 @@
+/* This file is necessary to suppress errors about import.meta.env */
+
+/// <reference types="vite/client" />
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    dts({
+      tsconfigPath: './tsconfig.json',
+      rollupTypes: false,
+      // entryRoot: './src/main.ts',
+      copyDtsFiles: true,
+      staticImport: true,
+      // declarationOnly: true,
+      outDir: 'dts',
+      // insertTypesEntry: true
+    }),
+    vue()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'BoilderPlateLib',
+      formats: ['es'],
+      fileName: 'boilerplate-dts-lib'
+    }
+  }
+})
